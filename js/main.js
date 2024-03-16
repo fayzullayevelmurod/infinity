5fabeca6db35b76b15e4437d173b8e84ab5c334f
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-03-16 11:02:17 +0500

    card more btn added

--- js/main.js
+++ js/main.js
@@ -5,6 +5,15 @@ let media_menu = document.querySelector('.media_menu');
 let media_menu_bg = document.querySelector('.media_menu_bg');
 let closeBtn = document.querySelector('.close');
 
+let card_more_btn = document.querySelectorAll('.card_more_btn');
+let team_card_text = document.querySelectorAll('.team_card_text');
+
+card_more_btn.forEach((btn, btnID) => {
+    btn.addEventListener('click', () => {
+        team_card_text[btnID].classList.toggle('active')
+    })
+})
+
 burger.addEventListener('click', () => {
     media_menu.classList.add('active')
     closeBtn.classList.add('active')
